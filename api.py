2a875576c61a8860b1132dd3ca8df8e668b4ce4c
MaulanaAlirridlo <59948773+MaulanaAlirridlo@users.noreply.github.com>
2021-12-20 12:05:15 +0700

    Fix typeerror

--- api.py
+++ api.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def store():
-    if request.is_json() :
+    if request.is_json :
         with open('store.json', 'w') as f:
             json.dump(request.get_json(), f)
         return jsonify({'status' : 200})
 
 if __name__ == '__main__':
-    app.run(host="47.254.240.15", debug=True, port=8000)
+    app.run(host="192.168.1.8", debug=True, port=8000)
